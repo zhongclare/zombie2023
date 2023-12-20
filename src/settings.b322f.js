93d73cb61db9b74e698a884a773d03c7e79930df
zhongclare <80366970+zhongclare@users.noreply.github.com>
2023-12-20 18:12:54 +0800

    12/20

---
+++ src/settings.b322f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"fb8a5",internal:"620a7",main:"316fc"}};
