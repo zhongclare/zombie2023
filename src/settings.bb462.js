e06a2e065c49100253be82efb1270dde2be952b2
zhongclare <80366970+zhongclare@users.noreply.github.com>
2024-01-04 18:33:09 +0800

    1/4

---
+++ src/settings.bb462.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"e8daf",internal:"620a7",main:"d1cb8"}};
