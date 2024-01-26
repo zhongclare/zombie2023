39eb00fad83cbc660b02047d57177624cf4a684c
zhongclare <80366970+zhongclare@users.noreply.github.com>
2024-01-26 18:52:25 +0800

    1/26

--- src/settings.0a092.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{resources:"5527f",internal:"620a7",main:"1ff2b"}};
