4d7aa4421c1174e9a778b36c05520dc1517ff951
zhongclare <80366970+zhongclare@users.noreply.github.com>
2024-02-02 18:27:21 +0800

    2/2

---
+++ src/settings.5eade.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"620a7",resources:"4c83f",main:"1ac82"}};
